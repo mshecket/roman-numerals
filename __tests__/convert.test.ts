a7422693d904c2f96b31cd807ed67b1fec1d3621
mshecket <mike@mikeshecket.com>
2020-09-21 00:27:13 -0400

    Worked my way up to 399

--- __tests__/convert.test.ts
+++ __tests__/convert.test.ts
@@ -1,31 +1,109 @@
-import convert from "../src/convert"
+import convert from "../src/convert";
 
 describe("Converting Roman numerals to Arabic numerals", () => {
-    test("I is converted to 1", () => {
-        expect(convert("I")).toBe(1);
-    });
-    test("II is converted to 2", () => {
-        expect(convert("II")).toBe(2);
-    });
-    test("IV is converted to 4", () => {
-        expect(convert("IV")).toBe(4);
-    });
-    test("V is converted to 5", () => {
-        expect(convert("V")).toBe(5);
-    });
-    test("VIII is converted to 8", () => {
-        expect(convert("VIII")).toBe(8);
-    });
-})
+  test("I is converted to 1", () => {
+    expect(convert("I")).toBe(1);
+  });
+  test("II is converted to 2", () => {
+    expect(convert("II")).toBe(2);
+  });
+  test("IV is converted to 4", () => {
+    expect(convert("IV")).toBe(4);
+  });
+  test("V is converted to 5", () => {
+    expect(convert("V")).toBe(5);
+  });
+  test("VIII is converted to 8", () => {
+    expect(convert("VIII")).toBe(8);
+  });
+  test("IX is converted to 9", () => {
+    expect(convert("IX")).toBe(9);
+  });
+  test("X is converted to 10", () => {
+    expect(convert("X")).toBe(10);
+  });
+  test("XIII is converted to 13", () => {
+    expect(convert("XIII")).toBe(13);
+  });
+  test("IIIX returns an error", () => {
+    expect(convert("IIIX")).toBe("error");
+  });
+  test("XX is converted to 20", () => {
+    expect(convert("XX")).toBe(20);
+  });
+  test("XXXIX is converted to 39", () => {
+    expect(convert("XXXIX")).toBe(39);
+  });
+  test("XL is converted to 40", () => {
+    expect(convert("XL")).toBe(40);
+  });
+  test("LV is converted to 55", () => {
+    expect(convert("LV")).toBe(55);
+  });
+  test("LXXXVIII is converted to 88", () => {
+    expect(convert("LXXXVIII")).toBe(88);
+  });
+  test("LXXXIX is converted to 89", () => {
+    expect(convert("LXXXIX")).toBe(89);
+  });
+  test("XCII is converted to 92", () => {
+    expect(convert("XCII")).toBe(92);
+  });
+  test("XCIX is converted to 99", () => {
+    expect(convert("XCIX")).toBe(99);
+  });
+  test("C is converted to 100", () => {
+    expect(convert("C")).toBe(100);
+  });
+  test("CCCLXXXVIII is converted to 388", () => {
+    expect(convert("CCCLXXXVIII")).toBe(388);
+  });
+});
 
 describe("Converting Arabic numerals to Roman numerals", () => {
-    test("1 is converted to I", () => {
-        expect(convert(1)).toBe("I");
-    });
-    test("2 is converted to II", () => {
-        expect(convert(2)).toBe("II");
-    });
-    test("3 is converted to III", () => {
-        expect(convert(3)).toBe("III");
-    });
-})
+  test("1 is converted to I", () => {
+    expect(convert(1)).toBe("I");
+  });
+  test("2 is converted to II", () => {
+    expect(convert(2)).toBe("II");
+  });
+  test("3 is converted to III", () => {
+    expect(convert(3)).toBe("III");
+  });
+  test("5 is converted to V", () => {
+    expect(convert(5)).toBe("V");
+  });
+  test("8 is converted to VIII", () => {
+    expect(convert(8)).toBe("VIII");
+  });
+  test("9 is converted to IX", () => {
+    expect(convert(9)).toBe("IX");
+  });
+  test("10 is converted to X", () => {
+    expect(convert(10)).toBe("X");
+  });
+  test("30 is converted to XXX", () => {
+    expect(convert(30)).toBe("XXX");
+  });
+  test("39 is converted to XIX", () => {
+    expect(convert(39)).toBe("XXXIX");
+  });
+  test("40 is converted to XL", () => {
+    expect(convert(40)).toBe("XL");
+  });
+  test("45 is converted to XLV", () => {
+    expect(convert(45)).toBe("XLV");
+  });
+  test("50 is converted to L", () => {
+    expect(convert(50)).toBe("L");
+  });
+  test("94 is converted to XCIV", () => {
+    expect(convert(94)).toBe("XCIV");
+  });
+  test("145 is converted to CXLV", () => {
+    expect(convert(145)).toBe("CXLV");
+  });
+  test("398 is converted to CCCXCVIII", () => {
+    expect(convert(398)).toBe("CCCXCVIII");
+  });
+});
--- src/convert.ts
+++ src/convert.ts
@@ -5,18 +5,50 @@ function convert(input: any): any {
   if (typeof input == "string") {
     if ((<string>input).length == 0) {
       return 0;
-    }
-    if (/^I+$/.test(<string>input)) {
+    } else if (/^I+$/.test(<string>input)) {
       return (<string>input).length;
     } else if (<string>input == "IV") {
       return 4;
     } else if (<string>input.slice(0, 1) == "V") {
-      return <number>5 + <number>convert(<string>input.slice(1));
+      return 5 + <number>convert(<string>input.slice(1));
+    } else if (<string>input == "IX") {
+      return 9;
+    } else if (<string>input.slice(0, 2) == "XL") {
+      return 40 + <number>convert(<string>input.slice(2));
+    } else if (<string>input.slice(0, 2) == "XC") {
+      return 90 + <number>convert(<string>input.slice(2));
+    } else if (<string>input.slice(0, 1) == "X") {
+      return 10 + <number>convert(<string>input.slice(1));
+    } else if (<string>input.slice(0, 1) == "L") {
+      return 50 + <number>convert(<string>input.slice(1));
+    } else if (<string>input.slice(0, 1) == "C") {
+      return 100 + <number>convert(<string>input.slice(1));
+    } else {
+      return "error";
     }
   } else if (typeof input == "number") {
-    if (<number>input < 4) return "I".repeat(<number>input);
-    else {
-      return "II";
+    if (<number>input == 0) {
+      return "";
+    } else if (<number>input < 4) {
+      return "I".repeat(<number>input);
+    } else if (<number>input == 4) {
+      return "IV";
+    } else if (<number>input < 9) {
+      return "V" + <string>convert(<number>input - 5);
+    } else if (<number>input == 9) {
+      return "IX";
+    } else if (<number>input < 40) {
+      return "X" + <string>convert(<number>input - 10);
+    } else if (<number>input < 50) {
+      return "XL" + <string>convert(<number>input - 40);
+    } else if (<number>input < 90) {
+      return "L" + <string>convert(<number>input - 50);
+    } else if (<number>input < 100) {
+      return "XC" + <string>convert(<number>input - 90);
+    } else if (<number>input < 400) {
+      return "C" + <string>convert(<number>input - 100);
+    } else {
+      return "error";
     }
   } else {
     return "error";
